e5b904397556539fda224d66ad62ae3583cb147a
Quentin Moss <quejmoss@gmail.com>
2017-01-10 21:49:09 -0800

    update

--- wiki.go
+++ wiki.go
@@ -3,11 +3,17 @@ package main
 import "io/ioutil"
 
 type Page struct {
-	Title string
-	Body  []byte
+  Title string
+  Body  []byte
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+  filename := title + ".txt"
+  body, _ := ioutil.ReadFile(filename)
+  return &Page{Title: title, Body: body}
 }
